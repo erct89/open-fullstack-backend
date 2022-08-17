2eaf55c59be5636e5ea0f41f7a9deec71bbc8c3d
emilio ag <emilio.anover.contractor@bbva.com>
2022-08-17 14:21:58 +0200

    feat(.eslintignore, .eslintrc.cjs, package.json, package-lock.json): add eslint to config [OFS-B03C]

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single',
+      { 'allowTemplateLiterals': true }
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'eqeqeq': [
+      'error',
+      'always'
+    ],
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ]
+  }
+};
