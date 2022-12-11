2c65001ce90fbb96ceb797121173428f7c786ef0
Emilio AG <emilio_rct@hotmail.es>
2022-12-11 13:12:05 +0100

    feat(users): modify users validations [OFS-04D]

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { check, body } from 'express-validator';
+import { body } from 'express-validator';
 import { validateUniqueUser } from '../helpers/validationDBUsers.helper.js';
 import validateFields from '../middlewares/validateFields.middleware.js';
 import userController from '../controllers/users.controller.js';
@@ -9,9 +9,9 @@ const router = new Router();
 router.get('/', userController.getUsers);
 
 router.post('/', [
-  body('email', 'Invalid date from user').normalizeEmail().isEmail().withMessage('Not is Email').custom(validateUniqueUser('email')),
-  body('name', 'Invalid date from user').exists().isLength({ min: 3 }).isString().custom(validateUniqueUser('name')),
-  check('password', 'Invalid date from user').exists().isLength({ min: 8 }).withMessage('Password minimum length is 8 chars').isString(),
+  body('email', 'Invalid date from user').exists().withMessage('Email is required').normalizeEmail().isEmail().withMessage('Not is Email').custom(validateUniqueUser('email')),
+  body('name', 'Invalid date from user').exists().withMessage('Name is required').isLength({ min: 3 }).isString().custom(validateUniqueUser('name')),
+  body('password', 'Invalid date from user').exists().withMessage('Password is required').isLength({ min: 8 }).withMessage('Password minimum length is 8 chars').isString(),
   validateFields
 ], userController.createUser);
 
